f526294085882ed0ca2928a79a8ff5eb4f00a2e9
nurlanhsnv <161680141+nurlanhsnv@users.noreply.github.com>
2024-10-25 11:00:37 +0400

    first commit

---
+++ src/main/kotlin/com/example/sdf1spring/Sdf1SpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sdf1spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sdf1SpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<Sdf1SpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sdf1spring/Sdf1SpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sdf1spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class  Sdf1SpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
